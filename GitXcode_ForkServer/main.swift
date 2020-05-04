b14c64bd9a64771524aa0c103f117a3f816b1826
bangyoon <yoonb720@naver.com>
2020-05-04 23:26:32 +0900

    Add Hello World 1 in team leader server

---
+++ GitXcode_ForkServer/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  GitXcode_ForkServer
+//
+//  Created by 방윤 on 2020/05/04.
+//  Copyright © 2020 BangYoon. All rights reserved.
+//
+
+import Foundation
+
+print("Hello World 1 in team leader server")
